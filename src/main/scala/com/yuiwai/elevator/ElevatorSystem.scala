5e602a50d1b47fa53e14d985133d6deba3a304df
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-11-15 07:14:17 +0900

    実装中

--- src/main/scala/com/yuiwai/elevator/ElevatorSystem.scala
+++ src/main/scala/com/yuiwai/elevator/ElevatorSystem.scala
@@ -1,40 +1,76 @@
 package com.yuiwai.elevator
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import com.yuiwai.elevator.Building.{BuildingEvent, BuildingInitialized, BuildingState}
+import com.yuiwai.elevator.Elevator.{ElevatorEvent, ElevatorInitialized}
+import com.yuiwai.elevator.ElevatorSystem.BuildingCallback
+import com.yuiwai.elevator.Passenger.PassengerMsg
 
 import scala.util.Random
 
-object ElevatorSystem {
+object EntryPoint {
   def main(args: Array[String]): Unit = {
-    val system = ActorSystem(apply(), "elevator-system")
+    val system = ActorSystem(ElevatorSystem(), "elevator-system")
   }
+}
 
+object ElevatorSystem {
   sealed trait SystemMsg
+  final case class BuildingCallback(event: BuildingEvent) extends SystemMsg
+
+  sealed trait PassengerGeneratorEvent extends SystemMsg
+  final case class PassengerGenerated(passengers: Seq[ActorRef[PassengerMsg]]) extends PassengerGeneratorEvent
+
   def apply(seed: Long = 1): Behavior[SystemMsg] = Behaviors.setup { ctx =>
     Random.setSeed(seed)
-    val building = ctx.spawn(Building.apply(1), "building")
+    val building = ctx.spawn(Building(BuildingState(1, ctx.self)), "building")
     Behaviors.receiveMessage {
+      case BuildingCallback(event) =>
+        event match {
+          case BuildingInitialized => Behaviors.same
+        }
       case _ => Behaviors.same
     }
   }
 }
 
 object Building {
+  final case class BuildingState(
+    numOfElevators: Int,
+    system: ActorRef[BuildingCallback]
+  )
+
+  sealed trait BuildingEvent
+  case object BuildingInitialized extends BuildingEvent
+
   sealed trait BuildingMsg
-  final case class Tick(time: Long)
-  def apply(numOfElevators: Int): Behavior[BuildingMsg] = Behaviors.setup { ctx =>
-    val elevators = (1 to numOfElevators).map { i =>
+  final case class Tick(time: Long) extends BuildingMsg
+
+  final case class ElevatorCallback(event: ElevatorEvent) extends BuildingMsg
+
+  def apply(state: BuildingState): Behavior[BuildingMsg] = Behaviors.setup { ctx =>
+    val elevators = (1 to state.numOfElevators).map { i =>
       ctx.spawn(Elevator.apply(), s"elevator_$i")
     }
+    state.system ! BuildingCallback(BuildingInitialized)
     Behaviors.receiveMessage {
+      case ElevatorCallback(event) =>
+        event match {
+          case ElevatorInitialized => Behaviors.same
+        }
       case _ => Behaviors.same
     }
   }
 }
 
 object Elevator {
+  final case class ElevatorState()
+  sealed trait ElevatorEvent
+  case object ElevatorInitialized extends ElevatorEvent
+
   sealed trait ElevatorMsg
+  final case class Enter(passenger: ActorRef[PassengerMsg]) extends ElevatorMsg
   def apply(currentFloor: Int = 1, targetFloor: Option[Int] = None): Behavior[ElevatorMsg] =
     Behaviors.receive { (ctx, msg) =>
       msg match {
@@ -44,10 +80,32 @@ object Elevator {
 }
 
 object Passenger {
+  final case class PassengerState()
+
+  sealed trait PassengerEvent
+  case object LeftBuilding extends PassengerEvent
+
   sealed trait PassengerMsg
-  def apply(goto: Int, wait: Int): Behavior[PassengerMsg] = Behaviors.receive { (ctx, msg) =>
-    msg match {
-      case _ => Behaviors.same
-    }
+  final case class Arrived(floor: Int) extends PassengerMsg
+  def apply(destination: Int, interval: Int, createdAt: Long): Behavior[PassengerMsg] =
+    Behaviors.setup(_ => waiting())
+  private def waiting(): Behavior[PassengerMsg] = Behaviors.receiveMessage {
+    case _ => Behaviors.same
+  }
+  private def stay(): Behavior[PassengerMsg] = Behaviors.receiveMessage {
+    case _ => Behaviors.same
+  }
+  private def back(): Behavior[PassengerMsg] = Behaviors.receiveMessage {
+    case _ => Behaviors.same
+  }
+}
+
+object PassengerGenerator {
+  sealed trait PassengerGeneratorMsg
+  sealed trait PassengerGeneratorCommand
+  final case class GeneratePassenger(time: Long) extends PassengerGeneratorCommand
+
+  def apply(): Behavior[PassengerGeneratorMsg] = Behaviors.receive {
+    (_, _) => Behaviors.same
   }
 }
